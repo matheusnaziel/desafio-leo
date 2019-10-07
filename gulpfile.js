934b3435fcbaf1a6be555744df3c42499a05e15d
Matheus Naziel <matheus.naziel@gmail.com>
2019-10-07 01:23:50 -0300

    compilando imagens e alterando estilo do slide.

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ var connect = require('gulp-connect');
 var less = require('gulp-less');
 var minify = require('gulp-minify');
 var htmlmin = require('gulp-htmlmin');
+var imagemin = require( 'gulp-imagemin' );
+var image = require( 'gulp-image' );
 // var concat = require('gulp-concat');
 
 gulp.task('connect', function() {
-	connect.server({
+  	connect.server({
 		root: 'dist',
 		livereload: true,
 		port: 8080
@@ -14,24 +16,31 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function() {
-	return gulp
-		.src('./src/index.html')
+	  return gulp.src('./src/index.html')
 		.pipe(htmlmin({ collapseWhitespace: true }))
 		.pipe(gulp.dest('./dist/'))
 		.pipe(connect.reload());
 });
 
 gulp.task('styles', function() {
-	return gulp.src('./src/styles/style.less')
-	.pipe(less()).pipe(gulp.dest('./dist/styles'))
-	.pipe(connect.reload());
+    return gulp.src('./src/styles/style.less')
+    .pipe(less())
+    .pipe(gulp.dest('./dist/styles'))
+    .pipe(connect.reload());
+});
+
+gulp.task('image', function() {
+    return gulp.src('./src/images/*')
+    .pipe(image())
+    .pipe(gulp.dest('./dist/images'))
+    .pipe(connect.reload());
 });
 
 gulp.task('scripts', function() {
-	return gulp.src('./src/scripts/**/*.js')
-	.pipe(minify())
-	.pipe(gulp.dest('./dist/scripts/'))
-	.pipe(connect.reload());
+    return gulp.src('./src/scripts/**/*.js')
+    .pipe(minify())
+    .pipe(gulp.dest('./dist/scripts/'))
+    .pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
@@ -40,4 +49,4 @@ gulp.task('watch', function() {
 	gulp.watch([ './src/scripts/**/*.js' ], gulp.series('scripts'));
 });
 
-gulp.task('default', gulp.parallel('html', 'styles', 'scripts', 'connect', 'watch'));
+gulp.task('default', gulp.parallel('html', 'styles', 'scripts', 'image', 'connect', 'watch'));
