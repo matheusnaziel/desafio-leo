ff427091694fa0c0186456f6be684ce00d5d7571
Matheus Naziel <matheus.naziel@gmail.com>
2019-10-08 23:10:17 -0300

    revisando variáveis no arquivo style.less, e criando modal.

---
+++ Readme.md
@@ -0,0 +1,3 @@
+Desafio Leo Learning
+
+Para rodar o projeto basta digitar o comando gulp.
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -3,34 +3,34 @@ var arrowLeft = document.querySelector("#arrow-left");
 var arrowRight = document.querySelector("#arrow-right");
 var current = 0;
 
-// Clear all images
+// Limpa todas as imagens
 function reset() {
   for (let i = 0; i < sliderImages.length; i++) {
     sliderImages[i].style.display = "none";
   }
 }
 
-// Init slider
+// Inicia o Slider
 function startSlide() {
   reset();
   sliderImages[0].style.display = "block";
 }
 
-// Show prev
+// Mostra o anterior
 function slideLeft() {
   reset();
   sliderImages[current - 1].style.display = "block";
   current--;
 }
 
-// Show next
+// Mostra o próximo
 function slideRight() {
   reset();
   sliderImages[current + 1].style.display = "block";
   current++;
 }
 
-// Left arrow click
+// Click seta esquerda
 arrowLeft.addEventListener("click", function() {
   if (current === 0) {
     current = sliderImages.length;
@@ -38,7 +38,7 @@ arrowLeft.addEventListener("click", function() {
   slideLeft();
 });
 
-// Right arrow click
+// // Click seta direita
 arrowRight.addEventListener("click", function() {
   if (current === sliderImages.length - 1) {
     current = -1;
@@ -47,3 +47,27 @@ arrowRight.addEventListener("click", function() {
 });
 
 startSlide();
+
+
+// Pegando o modal
+var modal = document.getElementById("myModal");
+
+// Pegando o elemento <span> para fechar o modal
+var span = document.getElementsByClassName("close")[0];
+
+// Quando a página carregar, abra o modal
+window.onload = function() {
+  modal.style.display = "block";
+}
+
+// Quando o usuário clicar no <span> (x), fecha o modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Quando o usuário clicar em qualquer lugar da página, fecha o modal
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
